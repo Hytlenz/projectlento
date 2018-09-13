a78ab57d4859ceb1c1676eb1ae6dd7cafe6fc3ef
Hyt <33175224+Hytlenz@users.noreply.github.com>
2018-09-14 07:09:37 +0800

    Learn1
    A Simple Tutorial 1

---
+++ learn1.java
@@ -0,0 +1,29 @@
+
+// importante tong nasa baba nato.
+import java.util.*;
+
+/*
+ * itoy comment na ito ay pampaganda lamang sa code
+ * @author Lento_Kun
+ */
+ 
+ // itong filename ay pwede ibahin for example "lento.java".
+ class learn1{
+    // wag kakalimutan tong mga to
+    public static void main(String[] args){
+    
+    // kaya may scanner dahil mag iinput ka
+    Scanner test = new Scanner(System.in);
+    
+    // variables ng java.
+    String text = "Welcome to Mobile Legends"; // String for text.
+    int num = 35; // int for whole number.
+    float flow = 7.0f; // di ko pa sya nagagamit pero parang ganyan siguro ang float.
+    double doble = 1.2; // double with float
+    
+    // for outputs
+    System.out.print(text + num + flow + doble);
+    // output is "Welcome to Mobile Legends 35 7.0f 1.2".
+    
+        }
+    }
