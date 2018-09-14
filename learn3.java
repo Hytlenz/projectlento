74ddbf74636d9332ce7f44890c16b9f66cb74c77
LentoKun <33175224+Hytlenz@users.noreply.github.com>
2018-09-14 13:03:52 +0800

    Update learn3.java

--- learn3.java
+++ learn3.java
@@ -1,13 +1,21 @@
 
-// TODO later
+// Gagawa tayo ng authentication login system
 
 import java.util.*;
 
 class learn3{
 
   public static void main(String[] args){
-       // TODO MAMAYA O BUKAS XD
-       
+    
+    // input again
+    Scanner input = new Scanner(System.in);
+    String user = "lento";
+    String pass = "kun";
+    
+    // eto yung loop na pabalik balik kapag hindi na execute ang tamang input
+    auth;
+    for(x = 0; ; x++);
+       // LATER NAKALIMUTAN KO GAGAWIN
        
        
      }
