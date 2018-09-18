ed54c51621ef6469bd5f2937d3fb14ab3db7616f
LentoKun <33175224+Hytlenz@users.noreply.github.com>
2018-09-18 20:39:34 +0800

    Learn3
    Ayan na updated na siya mga ka dyebs

--- learn3.java
+++ learn3.java
@@ -7,16 +7,26 @@ class learn3{
 
   public static void main(String[] args){
     
-    // input again
+    // input again.
     Scanner input = new Scanner(System.in);
-    String user = "lento";
+    String user = "lento"; // kelangan lento for user, kun for pass ang itatype mo para mapunta sa true statement ng if.
     String pass = "kun";
+    String a, b;
     
-    // eto yung loop na pabalik balik kapag hindi na execute ang tamang input
-    auth;
-    for(x = 0; ; x++);
-       // LATER NAKALIMUTAN KO GAGAWIN
+    // eto yung loop na pabalik balik kapag hindi na execute ang tamang input.
+    auth:
+    for(x = 0; ; x++){
+      System.out.print("Enter Username: "); //Yung itatype mo pag mali babalik sa gantong itsura.
+      a = input.next();
+      System.out.print("Enter Password: ");
+      b = input.next();
+      
+      if(a.equals(user) && b.equals(pass)){
+        System.out.println("Access Granted"); // pag tama eto lalabas.
+      } else {
+        System.out.println("Invalid User or Pass"); // pag mali babalik ka sa enter user and pass.
+      }
+    }
        
-       
-     }
+    }
  }
