f22bedb912abaed7af04a3575364809cda34e965
LentoKun <33175224+Hytlenz@users.noreply.github.com>
2018-10-08 17:27:27 +0800

    Update casestudy.java

--- casestudy.java
+++ casestudy.java
@@ -5,3 +5,26 @@ import java.util.*;
  */
  
  class casestudy{
+  
+  public static void main (String[] args){
+   
+   Scanner input = new Scanner (System.in);
+   
+   System.out.print("\n Enter your Watts");
+   float x = input.nextFloat();
+   
+   System.out.print("\n Enter your hours");
+   float y = input.nextFloat();
+   
+   float z = x*y;
+   float a = z/1000;
+   float b = a*30;
+   
+   System.out.print("\n Kilowatts is " + a);
+   if(a == 1){
+    System.out.print("1 Solar Panel");
+   } else if(a == 2){
+    System.out.print("2 Solar Panels");
+   }
+  }
+ }
