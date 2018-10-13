79e30536f24c81de8997125ab24748dbf744ec4d
LentoKun <33175224+Hytlenz@users.noreply.github.com>
2018-10-13 17:48:25 +0800

    Add Comments and Change Variables
    Nothing to do its complete. I need is example

--- Pascal.java
+++ Pascal.java
@@ -17,6 +17,7 @@ import java.util.*;
    
        System.out.println(title + bot + " The size of Solar Panel is 17.6 Square Feet \n(65 inch x 39k inch) \nAnd its have a 265 watts");
        System.out.print(bot +"Enter the Square Feet of your house \nto know how many solar panel \ncan fit into your roof!\n [ Input Here ]: ");
+       //Input Squre Feet to know how many solar panels can fit into your roof.
             int squarefeet = input.nextInt();
             int sp = (squarefeet/18);
       
@@ -25,7 +26,8 @@ import java.util.*;
        System.out.print("\nto know how many panels you'll need\n");
        System.out.print(bot + "\nPress r if rainy and s if sunny\n [ Input Here ]: ");
        String choice = input.next();
-                        
+       //Input r or s to know your season in your country.
+   
        if(choice.equalsIgnoreCase("r")){
        System.out.print(bot + "\n This is a Rainy Season \nWe need the 70% of your kwh\n So Enter your kwh per month\n [ Input Here ]: ");
              double kwh = input.nextDouble();
@@ -35,10 +37,10 @@ import java.util.*;
        System.out.println("Your kwh per day is "+ kwhperday +"!");
        System.out.print(bot +"The Sun Hours per day in Philippines is 5 hours!"+ bot +"Enter your Sunhours \n [ Input Here ]: ");
              double sunhours1 = input.nextDouble();
-             double solarpanelproduce1 = (265*sunhours1);
-             double solarpanelproduce2 = (solarpanelproduce1/1000);
+             double spp1 = (265*sunhours1);
+             double spp2 = (spp1/1000);
              double sp1 = (kwhperday/solarpanelproduce2);
-       
+       //spp means Solar Panel Produce and sp means Solar Panel.
        System.out.println(bot +"Your Solar Panel Your Need is "+ sp1);  
        
        }else if(choice.equalsIgnoreCase("s")){
@@ -48,10 +50,10 @@ import java.util.*;
        System.out.println("Your kwh per day is "+kwhh+"!");
        System.out.print(bot +"The Sun Hours per day in Philippines is 5 hours!"+ bot + "Enter Your Sunhours \n [ Input Here ]: ");
              double sunhours2 = input.nextDouble();
-             double solarpanelproduce3 = (265*sunhours2);
-             double solarpanelproduce4 = (solarpanelproduce3/1000);
-             double sp2 = (kwhh/solarpanelproduce4);
-       
+             double spp3 = (265*sunhours2);
+             double spp4 = (spp3/1000);
+             double sp2 = (kwhh/spp4);
+       //spp means Solar Panel Produce and sp means Solar Panel.
        System.out.println(bot +"Your Solar Panel Your Need is "+sp2);   
    }
   }
